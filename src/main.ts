5299dbf50fe94ffb1da9b82f932da5ab7f378c6a
Paulo <rick.souza97@gmail.com>
2022-02-25 18:06:37 -0300

    update main

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,9 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+
+  const port = process.env.PORT || 3000;
+
   const app = await NestFactory.create(AppModule);
   const config = new DocumentBuilder()
     .setTitle('CRUD Users example with NestJS and MySQL')
@@ -13,6 +16,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('swagger', app, document);
 
-  await app.listen(3000);
+  await app.listen(port);
 }
 bootstrap();
